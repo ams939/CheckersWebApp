3287380da30e58dc7a4b9ac42f6d3a47cb273651
Wonked <es854@drexel.edu>
2019-03-08 13:59:17 -0500

    Alex's format for movePiece packet

--- Server/main.py
+++ Server/main.py
@@ -54,10 +54,18 @@ def handleQuitGame(player, data):
        print(games)
 
 def handleMovePiece(player, data):
-    move = {'old_pos': {'row': data['pos'][0],
-                        'col': data['pos'][1]},
-            'new_pos': {'row': data['new_pos'][0],
-                        'col': data['new_pos'][1]}}
+    #move = {'old_pos': {'row': data['pos'][0],
+    #                    'col': data['pos'][1]},
+    #        'new_pos': {'row': data['new_pos'][0],
+    #                    'col': data['new_pos'][1]}}
+
+    move = {'old_pos': data['old_pos'],
+            'new_pos': data['new_pos']}
+
+    print(move)
+
+    #validate both player's session_ids are the same and they match the one in the packet
+    #validate the coordinates are integers
 
     #No validation yet
     sess = games.get(player.session_id, None)
@@ -71,10 +79,10 @@ def handleMovePiece(player, data):
 
        sess.change_turn()
 
-       packet = { 'session_id'   : sess.session_id,
-                  'current_turn' : sess.current_turn,
-                  'board'        : new_board.to_json(),
-                  'valid'        : True }
+       packet = {'session_id'   : sess.session_id,
+                 'current_turn' : sess.current_turn,
+                 'board'        : new_board.to_json(),
+                 'valid'        : True}
 
        player.get_websocket().sendMessage(buildPacket(2, packet))
        sess.get_player_two().get_websocket().sendMessage(buildPacket(2, packet))
@@ -82,7 +90,8 @@ def handleMovePiece(player, data):
        print('Player is not in a game')
 
 def handleJoinQueue(player, data):
-    #queue.append(DummyPlayer("Test1"))
+    queue.append(DummyPlayer("Test1"))
+    #todo: possible to double join queue
 
     player.get_websocket().sendMessage(buildPacket(3, {'success': True}))
 
