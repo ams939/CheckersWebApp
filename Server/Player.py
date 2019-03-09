4e61a40efc792e2ee0d211f77ac930f9061c0527
ams939 <ams939@drexel.edu>
2019-03-09 17:13:31 -0500

    Linted player.py

--- Server/Player.py
+++ Server/Player.py
@@ -1,3 +1,4 @@
+"""
 # Drexel University - CS451 Checkers Web Application
 # Authors: Alex Sladek, Brian Stump, Eric Szabo, Philip Stephenson
 # 3.3.2019
@@ -8,37 +9,56 @@
 # Python v. 3.7.1
 #
 #
+"""
 
 
-# Player object, initialized with username string and Websocket object as arguments
 class Player:
+    """
+    # Player object, initialized with username string and Websocket object as arguments
+    """
+
     def __init__(self, username, websocket):
         self.username = username
         self.websocket = websocket # Websocket object
         self.session_id = None
 
 
-    # Function for checking if user is still connected
-    def is_connected(self):
-        # TODO: Define function behavior for checking if user connected
-        return True
-
     def set_session_id(self, new_session_id):
+        """
+        :param new_session_id: String
+        :return: None
+        """
         self.session_id = new_session_id
 
     def get_session_id(self):
+        """
+        :return: String
+        """
         return self.session_id
 
+
     def get_websocket(self):
+        """
+        :return: Websocket Object
+        """
         return self.websocket
 
+
     def set_websocket(self, new_websocket):
+        """
+        :param new_websocket: Websocket object
+        :return: None
+        """
         self.websocket = new_websocket
 
+
     def to_json(self):
-        d = {}
-        d["username"] = self.username
-        d["session_id"] = self.session_id
-        return d
-
-
+        """
+        Converts object to json
+        :return: json object
+        """
+        player_dict = {}
+        player_dict["username"] = self.username
+        player_dict["session_id"] = self.session_id
+        return player_dict
+    
